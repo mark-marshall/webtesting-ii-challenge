b0a0ef882e0e67a8272995fd4f2a196b55779afb
mark-marshall <mark.marshallgp@gmail.com>
2019-04-09 14:21:06 +0100

    [MVP] Added Container testing for dashboard clicks on Add Strike, Add Ball, Add Foul, and Add Hit

--- baseball/src/components/Container.test.jsx
+++ baseball/src/components/Container.test.jsx
@@ -6,8 +6,99 @@ import Container from './Container';
 afterEach(rtl.cleanup);
 
 describe('<Container />', () => {
-    it('renders the h2 with the apps title', () => {
-        const wrapper = rtl.render(<Container />);
-        expect(wrapper.getByText(/baseball/i))
-    })
-})
+  it('renders the h2 with the apps title', () => {
+    const wrapper = rtl.render(<Container />);
+    expect(wrapper.getByText(/baseball/i));
+  });
+  // STRIKES TESTING
+  it('increases strike by 1 when strikes is 0', () => {
+    const wrapper = rtl.render(<Container />);
+    expect(wrapper.getByText(/strikes: 0/i));
+    rtl.fireEvent.click(wrapper.getByText(/add strike/i));
+    expect(wrapper.getByText(/strikes: 1/i));
+  });
+  it('strikes are reset to 0 after 3rd strike', () => {
+    const wrapper = rtl.render(<Container />);
+    expect(wrapper.getByText(/strikes: 0/i));
+    rtl.fireEvent.click(wrapper.getByText(/add strike/i));
+    rtl.fireEvent.click(wrapper.getByText(/add strike/i));
+    rtl.fireEvent.click(wrapper.getByText(/add strike/i));
+    expect(wrapper.getByText(/strikes: 3/i));
+    rtl.fireEvent.click(wrapper.getByText(/add strike/i));
+    expect(wrapper.getByText(/strikes: 0/i));
+  });
+  it('balls are reset to 0 after 3rd strike', () => {
+    const wrapper = rtl.render(<Container />);
+    expect(wrapper.getByText(/balls: 0/i));
+    rtl.fireEvent.click(wrapper.getByText(/add ball/i));
+    expect(wrapper.getByText(/balls: 1/i));
+    rtl.fireEvent.click(wrapper.getByText(/add strike/i));
+    rtl.fireEvent.click(wrapper.getByText(/add strike/i));
+    rtl.fireEvent.click(wrapper.getByText(/add strike/i));
+    rtl.fireEvent.click(wrapper.getByText(/add strike/i));
+    expect(wrapper.getByText(/balls: 0/i));
+  });
+  // BALLS TESTING
+  it('increases ball by 1 when ball is 0', () => {
+    const wrapper = rtl.render(<Container />);
+    expect(wrapper.getByText(/balls: 0/i));
+    rtl.fireEvent.click(wrapper.getByText(/add ball/i));
+    expect(wrapper.getByText(/balls: 1/i));
+  });
+  it('balls are reset to 0 after 4th ball', () => {
+    const wrapper = rtl.render(<Container />);
+    expect(wrapper.getByText(/balls: 0/i));
+    rtl.fireEvent.click(wrapper.getByText(/add ball/i));
+    rtl.fireEvent.click(wrapper.getByText(/add ball/i));
+    rtl.fireEvent.click(wrapper.getByText(/add ball/i));
+    rtl.fireEvent.click(wrapper.getByText(/add ball/i));
+    expect(wrapper.getByText(/balls: 4/i));
+    rtl.fireEvent.click(wrapper.getByText(/add ball/i));
+    expect(wrapper.getByText(/balls: 0/i));
+  });
+  it('strikes are reset to 0 after 4th ball', () => {
+    const wrapper = rtl.render(<Container />);
+    expect(wrapper.getByText(/strikes: 0/i));
+    rtl.fireEvent.click(wrapper.getByText(/add strike/i));
+    expect(wrapper.getByText(/strikes: 1/i));
+    rtl.fireEvent.click(wrapper.getByText(/add ball/i));
+    rtl.fireEvent.click(wrapper.getByText(/add ball/i));
+    rtl.fireEvent.click(wrapper.getByText(/add ball/i));
+    rtl.fireEvent.click(wrapper.getByText(/add ball/i));
+    rtl.fireEvent.click(wrapper.getByText(/add ball/i));
+    expect(wrapper.getByText(/strikes: 0/i));
+  });
+  // FOUL TESTING
+  it('strike increases by 1 when strikes are less than 2 and a foul', () => {
+    const wrapper = rtl.render(<Container />);
+    expect(wrapper.getByText(/strikes: 0/i));
+    rtl.fireEvent.click(wrapper.getByText(/add foul/i));
+    expect(wrapper.getByText(/strikes: 1/i));
+  });
+  it('strike stays at 2 after a foul', () => {
+    const wrapper = rtl.render(<Container />);
+    expect(wrapper.getByText(/strikes: 0/i));
+    rtl.fireEvent.click(wrapper.getByText(/add strike/i));
+    rtl.fireEvent.click(wrapper.getByText(/add strike/i));
+    expect(wrapper.getByText(/strikes: 2/i));
+    rtl.fireEvent.click(wrapper.getByText(/add foul/i));
+    expect(wrapper.getByText(/strikes: 2/i));
+  });
+  // HIT TESTING
+  it('strikes and are reset to 0 after hit', () => {
+    const wrapper = rtl.render(<Container />);
+    expect(wrapper.getByText(/strikes: 0/i));
+    rtl.fireEvent.click(wrapper.getByText(/add strike/i));
+    expect(wrapper.getByText(/strikes: 1/i));
+    rtl.fireEvent.click(wrapper.getByText(/add hit/i));
+    expect(wrapper.getByText(/strikes: 0/i));
+  });
+  it('balls and are reset to 0 after hit', () => {
+    const wrapper = rtl.render(<Container />);
+    expect(wrapper.getByText(/balls: 0/i));
+    rtl.fireEvent.click(wrapper.getByText(/add ball/i));
+    expect(wrapper.getByText(/balls: 1/i));
+    rtl.fireEvent.click(wrapper.getByText(/add hit/i));
+    expect(wrapper.getByText(/balls: 0/i));
+  });
+});
--- baseball/src/components/Dashboard.jsx
+++ baseball/src/components/Dashboard.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function Dashboard({ fireStrike, fireBall, fireFoul, fireHit }) {
     return (
         <>
-         <button onClick={fireStrike}>Strike</button>
-         <button onClick={fireBall}>Ball</button>
-         <button onClick={fireFoul}>Foul</button>
-         <button onClick={fireHit}>Hit</button>
+         <button onClick={fireStrike}>Add Strike</button>
+         <button onClick={fireBall}>Add Ball</button>
+         <button onClick={fireFoul}>Add Foul</button>
+         <button onClick={fireHit}>Add Hit</button>
         </>
     )
 }
